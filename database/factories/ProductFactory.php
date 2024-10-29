ab139156010ee05ce105037f5b8f94695efd75bd
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-10-29 15:57:15 +0100

    Fin Paso 4

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,10 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->word(),
+            'category_id' => Category::inRandomOrder()->first()->id,
+            'description' => fake()->paragraph(),
+            'price' => rand(1000, 99999)
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,8 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         Category::factory(10)->create();
+        Product::factory(20)->create();
     }
 }
