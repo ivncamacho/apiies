3ac029ddb4a7de86a1feb610997b1268f496d139
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-11-08 15:57:05 +0100

    Fin Paso 13

--- routes/api.php
+++ routes/api.php
@@ -11,11 +11,8 @@ Route::get('/user', function (Request $request) {
 
 Route::get('lists/categories', [CategoryController::class,  'list']);
 
-//Route::get('categories', [CategoryController::class,  'index']);
-//Route::get('categories/{category}', [CategoryController::class,  'show']);
-//Route::post('categories', [CategoryController::class,  'store']);
-//Route::put('categories/{category}', [CategoryController::class,  'update']);
-//Route::delete('categories/{category}', [CategoryController::class,  'destroy']);
-Route::apiResource('categories', CategoryController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
 
-Route::get('products', [ProductController::class,  'index']);
+    Route::get('products', [ProductController::class,  'index']);
+});
