ff948a9211f510316881a6a9c467cbd4963a499e
LAPTOP-FIK1QKO4\ivan <1891569@alu.murciaeduca.es>
2024-11-19 15:56:47 +0100

    fin paso 17 creacion de comentarios para que se generen en la documentacion

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -8,10 +8,25 @@ use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Knuckles\Scribe\Attributes\Endpoint;
+use Knuckles\Scribe\Attributes\Group;
+use Knuckles\Scribe\Attributes\QueryParam;
 use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @group Categories
+ *
+ * Managing categories
+ */
 
 class CategoryController extends Controller
 {
+    /**
+     * Get all categories
+     *
+     * Getting the list of the categories
+     * */
+
     public function index()
     {
         abort_if(! auth()->user()->tokenCan('categories-list'), 403);
@@ -19,6 +34,7 @@ class CategoryController extends Controller
         return CategoryResource::collection(Category::all());
     }
 
+    #[Endpoint('Show category', description: 'Get a category by ID')]
     public function show(Category $category)
     {
         abort_if(! auth()->user()->tokenCan('categories-show'), 403);
@@ -26,6 +42,13 @@ class CategoryController extends Controller
         return new CategoryResource($category);
     }
 
+    /**
+     * Store a new category
+     *
+     * Creating a new category
+     *
+     * @bodyParam name string required The name of the category. Example: Electronics
+     */
     public function store(StoreCategoryRequest $request)
     {
         $data = $request->all();
--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,7 +6,18 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Knuckles\Scribe\Attributes\Group;
+use Knuckles\Scribe\Attributes\QueryParam;
 
+/**
+ * @group Products
+ *
+ * Managing products
+ *
+ * @queryParam page int The page number. Example: 1
+ */
+#[Group('Categories', description: 'Managing categories')]
+#[QueryParam('page', 'int', 'The page number', example: 1)]
 class ProductController extends Controller
 {
     public function index()
